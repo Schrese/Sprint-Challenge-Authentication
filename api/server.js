5eafcfe3ac0002294cc4e68da86895080b371742
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-31 11:15:58 -0500

    Changed port to a 5000, Added in base get request, Answered questions

--- api/server.js
+++ api/server.js
@@ -15,4 +15,10 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+    res.send(
+        '<h2>Question: What did the drummer call his twin daughters?</h2>\n<h5>Answer: Anna one, Anna two!</h5>'
+        )
+})
+
 module.exports = server;
